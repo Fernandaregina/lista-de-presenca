44249d0bd4967553779d49fc93318912453bb20a
Fernanda Regina de Souza Ferreira <ferrsouza2@hotmail.com>
2023-06-13 17:45:45 -0300

    inclusão css global
    inclusão css global

---
+++ lista/src/pages/App.jsx
@@ -0,0 +1,15 @@
+
+
+export function App() {
+  return(
+    <div>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite seu Nome" />
+      <button type="button">Adicionar</button>
+    </div>
+  )
+  
+  
+}
+
+
